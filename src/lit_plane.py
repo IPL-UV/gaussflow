35e83270ca92a0a54ae79bb07a9ad905f7ab40f7
jejjohnson <emanjohnson91@gmail.com>
2021-06-27 20:54:25 +0200

    updated flowplane model.

--- src/lit_plane.py
+++ src/lit_plane.py
@@ -48,7 +48,7 @@ class FlowLearnerPlane(pl.LightningModule):
         return {'loss': loss, 'log': logs}
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.cfg.learning_rate, betas=(0.9, 0.999), weight_decay=0.0)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.cfg.learning_rate, betas=(0.9, 0.999), weight_decay=1e-4)
         lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
             optimizer, 
             milestones=[
