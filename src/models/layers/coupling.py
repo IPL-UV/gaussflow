375c1a118db47ae199d07aaba096315311200df7
jejjohnson <emanjohnson91@gmail.com>
2021-07-27 10:59:51 +0200

    small changes.

--- src/models/layers/coupling.py
+++ src/models/layers/coupling.py
@@ -1,4 +1,24 @@
+from typing import Callable, Union
 import FrEIA.modules as Fm
+
+
+class FlowCouplingBlock:
+    def __init__(
+        self,
+        dims_in,
+        dims_c=[],
+        subnet_constructor: Callable = None,
+        bijector: Callable = None,
+        clamp: float = 2.0,
+        clamp_activation: Union[str, Callable] = "ATAN",
+    ):
+        super().__init__(dims_in, dims_c, clamp, clamp_activation)
+
+    def _coupling1(self, x1, u2, rev=False):
+        pass
+
+    def _coupling2(self, x2, u1, rev=False):
+        pass
 
 
 def get_coupling_layer(coupling: str = "glow"):
--- src/models/layers/rqs.py
+++ src/models/layers/rqs.py
@@ -1,26 +1,67 @@
-from typing import List, Optional
+from typing import List, Optional, Callable
 import FrEIA.modules as Fm
 from nflows import transforms
 
-class RQSplines(Fm.InvertibleModule):
 
+class CouplingRQSplines(Fm.InvertibleModule):
     def __init__(
-        self, 
-        dims_in: List[int], 
-        num_bins: int=10, 
-        tails: Optional[str]=None, 
-        tail_bound: float=1.0, 
-        identity_init: bool=False,
-        ):
+        self,
+        dims_in: List[int],
+        mask: List[int],
+        subnet_constructor: Callable,
+        num_bins: int = 10,
+        tails: Optional[str] = None,
+        tail_bound: float = 1.0,
+        **kwargs
+    ):
+        super().__init__(dims_in)
+
+        self.transform = transforms.PiecewiseRationalQuadraticCouplingTransform(
+            mask=mask,
+            transform_net_create_fn=subnet_constructor,
+            num_bins=num_bins,
+            tails=tails,
+            tail_bound=tail_bound,
+            **kwargs
+        )
+
+    def forward(self, x, rev=False, jac=True):
+        # the Jacobian term is trivial to calculate so we return it
+        # even if jac=False
+
+        # x is passed to the function as a list (in this case of only on element)
+        x = x[0]
+        if not rev:
+            # forward operation
+            x, log_jac_det = self.transform.forward(x)
+        else:
+            # backward operation
+            x, log_jac_det = self.transform.inverse(x)
+
+        return (x,), log_jac_det
+
+    def output_dims(self, input_dims):
+        return input_dims
+
+
+class RQSplines(Fm.InvertibleModule):
+    def __init__(
+        self,
+        dims_in: List[int],
+        num_bins: int = 10,
+        tails: Optional[str] = None,
+        tail_bound: float = 1.0,
+        identity_init: bool = False,
+    ):
         super().__init__(dims_in)
 
         self.transform = transforms.PiecewiseRationalQuadraticCDF(
-            shape=dims_in[0], 
-            num_bins=num_bins, 
-            tails=tails, 
+            shape=dims_in[0],
+            num_bins=num_bins,
+            tails=tails,
             tail_bound=tail_bound,
-            identity_init=identity_init
-            )
+            identity_init=identity_init,
+        )
 
     def forward(self, x, rev=False, jac=True):
         # the Jacobian term is trivial to calculate so we return it
