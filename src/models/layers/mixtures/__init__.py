d201dc0395ff0cfbe7515b38825d5fd6b6e50385
jejjohnson <emanjohnson91@gmail.com>
2021-09-02 01:23:33 +0200

    updated mixture dists * both gauss and logistic work fine

---
+++ src/models/layers/mixtures/__init__.py
@@ -0,0 +1,4 @@
+from src.models.layers.mixtures.mixture_gaussian import GaussianMixtureCDF
+from src.models.layers.mixtures.mixture_logistic import LogisticMixtureCDF
+
+__all__ = ["GaussianMixtureCDF", "LogisticMixtureCDF"]
