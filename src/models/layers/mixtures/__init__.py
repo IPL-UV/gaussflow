e1f92c69c1a072bf148bbd2b8d0f715f65ffbc7b
jejjohnson <emanjohnson91@gmail.com>
2021-09-02 21:22:26 +0200

    changed mixture coupling names.

--- src/models/layers/mixtures/__init__.py
+++ src/models/layers/mixtures/__init__.py
@@ -1,5 +1,5 @@
 from src.models.layers.mixtures.mixture_gaussian import GaussianMixtureCDF
 from src.models.layers.mixtures.mixture_logistic import LogisticMixtureCDF
-from src.models.layers.mixtures.coupling import MixtureGaussianCDFCoupling
+from src.models.layers.mixtures.mixture_coupling import MixtureGaussianCDFCoupling
 
 __all__ = ["GaussianMixtureCDF", "LogisticMixtureCDF", "MixtureGaussianCDFCoupling"]
